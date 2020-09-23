9aded21494b4648b57983cf3de9ca8afce47d34d
Jan Bajec <48645043+JanFeriAccount10@users.noreply.github.com>
2020-09-24 00:12:27 +0200

    največja glupost

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
+
+  myRange = {
+    lower: 40,
+    upper: 50
+  };
 
   constructor() {}
+
+  ngOnInit() {
+  }
+
+  refresh(mylower: number, myupper: number){
+    this.myRange = {
+      lower: mylower,
+      upper: myupper
+    };
+  }
 
 }
